8ccd7bfb3a91fe247367946c9fbc9c8bb28e7f23
maggie <langling10@163.com>
2017-05-05 00:17:30 +0800

    learnGulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,12 @@ var uglify 		= require('gulp-uglify');//压缩js
 var minify 		= require('gulp-minify-css');//压缩css
 var sass   		= require('gulp-sass');
 var browserSync = require('browser-sync');//自动刷新
+var useref		= require('gulp-useref');//将多个文件拼接成一个文件
+var gulpIf 		= require('gulp-if');//
+var imagemin	= require('gulp-imagemin');//图片压缩
+var cache		= require('gulp-cache');//减少图片重复压缩
+var del			= require('del');//清理生成文件
+
 
 gulp.task('testLess',function(){
 	gulp.src(['src/less/*.less'])
@@ -38,9 +44,38 @@ gulp.task('browserSync',function(){
 })
 
 
+gulp.task('useref',function(){
+	return gulp.src('src/pages/*.html')
+			.pipe(gulpIf('*.css',minify()))
+			.pipe(gulpIf('*.js',uglify()))
+			.pipe(useref())
+			.pipe(gulp.dest('dist/js/'));
+})
+
+gulp.task('images',function(){
+	return gulp.src('src/images/*.+(png|jpg|gif)')
+			.pipe(cache(imagemin({
+				interlaced: true
+			})))
+			.pipe(gulp.dest('dist/images'))
+})
+
+gulp.task('clean',function(callback){
+	del('dist');
+	return cache.clearAll(callback);
+})
+
+gulp.task('clean:dist',function(callback){
+	del([
+		'dist/**/*','!dist/images','!dist/images/**/*'],callback)
+})
+
+
 
 gulp.task('watch',['browserSync','sass'],function(){
 	gulp.watch('src/sass/*.scss',['sass']);
+	gulp.wathc('src/pages/*.html');
+	gulp.watch('src/js/*.js');
 })
 
 gulp.task('watchLess',function(){
@@ -52,3 +87,28 @@ gulp.task('watchLess',function(){
 gulp.task('default',['testLess','compass','sass'],function(){
 	console.log('hello world')
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
-var arr = [ 1,2,3,4,5,6];
-console.log(arr);
-var str = 'langlign@qq.com';
-var reg=/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((.[a-zA-Z0-9_-]{2,3}){1,2})$/;
-reg.test(str);
+var arr=[1,2,3,4,5,6];
+
+// console.log(arr);
+
+// var str="langlign@qq.com",
+
+// reg=/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((.[a-zA-Z0-9_-]{2,3}){1,2})$/;
+// reg.test(str);
